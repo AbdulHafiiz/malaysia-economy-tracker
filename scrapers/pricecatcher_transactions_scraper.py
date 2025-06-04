4b7eddfdb1835083d9c4538d74b1cf44b577c968
AbdulHafiiz <arhafiiz@gmail.com>
2025-06-04 12:54:05 +0800

    changed open mode from read to write

--- scrapers/pricecatcher_transactions_scraper.py
+++ scrapers/pricecatcher_transactions_scraper.py
@@ -26,7 +26,7 @@ GCP_DATASET_NAME = os.getenv('GCP_DATASET_NAME')
 
 AUTH_PATH = Path(FILEPATH / 'secrets' / os.getenv('SERVICE_ACCOUNT_FILE'))
 if not AUTH_PATH.exists():
-    with open(AUTH_PATH, 'r') as f:
+    with open(AUTH_PATH, 'w') as f:
         f.write(os.getenv(os.getenv('SERVICE_ACCOUNT_FILE')))
 
 write_client = bigquery.Client.from_service_account_json(FILEPATH / 'secrets' / os.getenv('SERVICE_ACCOUNT_FILE'))
