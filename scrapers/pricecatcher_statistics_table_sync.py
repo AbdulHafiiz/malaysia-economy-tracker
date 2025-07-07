857fa3393e62aad77de2b4ec525f385cb797e24e
AbdulHafiiz <arhafiiz@gmail.com>
2025-07-07 15:30:52 +0800

    added in try-catch segment to partial data truncation

--- scrapers/pricecatcher_statistics_table_sync.py
+++ scrapers/pricecatcher_statistics_table_sync.py
@@ -17,19 +17,26 @@ dataset = client.dataset(GCP_DATASET_NAME)
 
 
 def sync_district_table(period:Literal['weekly', 'monthly']):
-    # Clear current period's data and reinsert the data (since there's no easy way to update the median)
+    period_str = 'WEEK' if period == 'weekly' else 'MONTH'
     current_week = (pd.to_datetime('now') - pd.to_timedelta(pd.to_datetime('now').weekday())).date().strftime('%Y-%m-%d')
+
+    # Clear current period's data and reinsert the data (since there's no easy way to update the median)
     partial_data_query = f'''
     DELETE `{GCP_PROJECT_NAME}.{GCP_DATASET_NAME}.stats_{period}_district_pricecatcher_transactions`
-    WHERE {period_str.lower()}_start = {current_week}
+    WHERE {period_str.lower()}_start = @current_week
     '''
-    partial_res = client.query_and_wait(partial_data_query)
-    print(partial_res)
+    try:
+        partial_res = client.query_and_wait(
+            partial_data_query,
+            job_config=bigquery.QueryJobConfig(query_parameters=[bigquery.ScalarQueryParameter('current_week', 'STRING', current_week)])
+        )
+        print(partial_res)
+    except:
+        pass
 
-    period_str = 'WEEK' if period == 'weekly' else 'monthly'
     query = f'''
     INSERT INTO `{GCP_PROJECT_NAME}.{GCP_DATASET_NAME}.stats_{period}_district_pricecatcher_transactions` (
-        {period_str.lower()}_start, state, district, premise_type, item_code, min_price, max_price, median_price, mean_price
+        {period_str.lower()}_start, state, district, premise_type, item_code, min_price, max_price, mean_price
     )
     WITH unmoved_data AS (
         SELECT *
@@ -42,7 +49,6 @@ def sync_district_table(period:Literal['weekly', 'monthly']):
     SELECT
         TIMESTAMP_TRUNC(date, {period_str}) AS {period_str.lower()}_start, ppl.state, ppl.district, ppl.premise_type, unmoved_data.item_code,
         MIN(price) AS min_price, MAX(price) AS max_price,
-        APPROX_QUANTILES(price, 2)[OFFSET(1)] AS median_price,
         ROUND(AVG(price), 2) AS average_price
     FROM unmoved_data
     LEFT JOIN `{GCP_DATASET_NAME}.pricecatcher_premise_lookup` AS ppl
